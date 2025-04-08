9421c65526194a9a427894efaa73209271242f15
Anish <36662022+akp10110@users.noreply.github.com>
2025-04-08 23:22:04 +1000

    added helper utils.py

--- code/nn_classification/Workflow.py
+++ code/nn_classification/Workflow.py
@@ -2,6 +2,8 @@ import CircleModelV0
 import torch
 from torch import nn
 import data_classification as data
+from helper_utils import plot_decision_boundary
+import matplotlib.pyplot as plt
 
 model_0 = CircleModelV0.CircleModelV0()
 print(f"=== model_0.state_dict() === \n {model_0.state_dict()}\n\n")
@@ -36,7 +38,7 @@ def accuracy_fn(y_true, y_pred):
 torch.manual_seed(42)
 
 # Set the number of epochs
-epochs = 100
+epochs = 1000
 
 # Build training and evaluation loop
 for epoch in range(epochs):
@@ -75,3 +77,13 @@ for epoch in range(epochs):
         # Print out whats happening
         if epoch % 5 == 0:
             print(f"Epoch: {epoch} | Loss: {loss:.5f} | Accuracy: {accuracy:.2f}% | Test Loss: {test_loss:.5f} | Test Accuracy: {test_accuracy:.2f}%")
+
+# Plot decision boundary for training and test sets
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model=model_0, X=data.X_train, y=data.y_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model=model_0, X=data.X_test, y=data.y_test)
+print("1")
